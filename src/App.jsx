e6d78b728d586b35d4309d655888f2167927397c
yosef-tutian <yoseftutian@gmail.com>
2024-07-15 17:19:03 +0300

    Add CartContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react';
-
-import Header from './components/Header.jsx';
-import Shop from './components/Shop.jsx';
-import { DUMMY_PRODUCTS } from './dummy-products.js';
+import { useState } from "react";
+import Product from "./components/Product.jsx";
+import Header from "./components/Header.jsx";
+import Shop from "./components/Shop.jsx";
+import { DUMMY_PRODUCTS } from "./dummy-products.js";
+import { CartContext } from "./store/shopping-cart-context.jsx";
 
 function App() {
   const [shoppingCart, setShoppingCart] = useState({
@@ -65,14 +66,24 @@ function App() {
     });
   }
 
+  const ctxValue = {
+    items: shoppingCart.items,
+    addItemToCart: handleAddItemToCart,
+  };
   return (
-    <>
+    <CartContext.Provider value={ctxValue}>
       <Header
         cart={shoppingCart}
         onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
       />
-      <Shop onAddItemToCart={handleAddItemToCart} />
-    </>
+      <Shop />
+      {DUMMY_PRODUCTS.map((product) => (
+        <li key={product.id}>
+          <Product {...product} onAddToCart={handleAddItemToCart} />
+        </li>
+      ))}
+      <Shop />
+    </CartContext.Provider>
   );
 }
 
